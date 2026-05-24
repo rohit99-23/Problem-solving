71349b6212f54338ec710d75221fd101ce5ed9eb
Rohit kumar <144412423+rohit99-23@users.noreply.github.com>
2026-05-24 22:31:14 +0530

    Problem solving Day 2

---
+++ Day-2/Prob_6.cpp
@@ -0,0 +1,12 @@
+// Convert Celsius to Fahrenheit 
+#include <iostream>
+using namespace std;
+int main(){
+    float cel;
+    cout<<"Enter the temp in Celcius: ";
+    cin>>cel;
+
+    float Far=(cel*1.8)+32;
+
+    cout<<"your Temp in Farenheight is: "<<Far<<endl;
+}
