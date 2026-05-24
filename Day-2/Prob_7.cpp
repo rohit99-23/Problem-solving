596085db756d3de35bcb0a286fe9ac1caf9d15a2
Rohit kumar <144412423+rohit99-23@users.noreply.github.com>
2026-05-24 23:17:59 +0530

    Problem solving Day 2

---
+++ Day-2/Prob_7.cpp
@@ -0,0 +1,53 @@
+// Find area of rectangle, Square or Circle
+#include <iostream>
+using namespace std;
+
+void rect(){
+    int length, breadth;
+    cout<<"Enter Length & Breadth of Rectangle: ";
+    cin>>length>>breadth;
+
+    cout<<"Area of your Rectangle is: "<<length*breadth;
+}
+
+void sqr(){
+    double side;
+    cout<<"Enter side of Square: ";
+    cin>>side;
+
+    double Area=side*side;
+    cout<<"Area of your Rectangle is: "<<Area;
+
+}
+
+void cir(){
+    double radius;
+    cout<<"Enter Radius of Circle: ";
+    cin>>radius;
+
+    double Area=3.14*radius*radius;
+    cout<<"Area of your Circle is: "<<Area;
+
+}
+
+
+int main(){
+    int a;
+    cout<<"What you want to check Enter the number of your choice:"<<endl;
+    cout<<"Press 1 for Area of Rectangle"<<endl;
+    cout<<"Press 2 for Area of Square"<<endl;
+    cout<<"Press 3 for Area of Circle"<<endl;
+    cin>>a;
+
+    if (a == 1){
+        rect();
+    }else if(a == 2){
+        sqr();
+    }else if(a == 3){
+        cir();
+    }else{
+        cout<<"Enter the correct Choice";
+    }
+
+    return 0;
+}
