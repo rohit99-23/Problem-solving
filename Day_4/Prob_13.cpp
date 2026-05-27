fac592bec239abcbf13d4cf6089c39348b481ea2
Rohit kumar <144412423+rohit99-23@users.noreply.github.com>
2026-05-27 23:52:38 +0530

    Problem solving Day 4

---
+++ Day_4/Prob_13.cpp
@@ -0,0 +1,16 @@
+// Check leap year 
+#include <iostream>
+using namespace std;
+int main(){
+    int year;
+
+    cout<<"Enter year: ";
+    cin>>year;
+
+    if(year%4==0){
+        cout<<year<<" is Leap year";
+    }else{
+        cout<<year <<" is Normal year";
+    }
+    return 0;
+}
