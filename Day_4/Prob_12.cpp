f2116f2dd2b34e89f0054932869f2df84443a0b1
Rohit kumar <144412423+rohit99-23@users.noreply.github.com>
2026-05-27 23:43:53 +0530

    Problem solving Day 4

--- Day_4/Prob_12.cpp
+++ Day_4/Prob_12.cpp
@@ -7,11 +7,11 @@ int main(){
     cout<<"Enter Three Numbers: ";
     cin>>a>>b>>c;
 
-    if(a>b && a>c){
+    if(a>b && a>c || a==b && a>c || a==c && a>b){
         cout<<"Largest Number is: "<<a;
-    }else if(b>a && b>c){
+    }else if(b>a && b>c ){
         cout<<"Largest number is: "<<b;
-    }else if(c>a && c>a){
+    }else if(c>a && c>b || b==c && b>a){
         cout<<"Largest number is: "<<c;
     }else{
         cout<<"You have entered Equal number";
