8376c0455bd5810f1bdf342c6a7dc067cfc56902
Rohit kumar <144412423+rohit99-23@users.noreply.github.com>
2026-05-28 14:26:31 +0530

    Problem solving Day 5

---
+++ Day_5/Prob_15.cpp
@@ -0,0 +1,18 @@
+// Print multiplicaƟon table
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter number of which you want multiplication table: ";
+    cin>>a;
+
+    int mul=1;
+    cout<<"Multiplication table of: "<<a<<" is"<<endl;
+    for(int i=1; i<=10; i++){
+        mul=i*a;
+        cout<<mul<<endl;
+    }
+    return 0;
+
+}
