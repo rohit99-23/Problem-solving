503156debbc2fa08680eb7198d10b6aa26a3589c
Rohit kumar <144412423+rohit99-23@users.noreply.github.com>
2026-05-28 15:31:22 +0530

    Problem solving Day 5

--- Day_5/Prob_19.cpp
+++ Day_5/Prob_19.cpp
@@ -7,10 +7,12 @@ int main(){
     cout<<"Enter a number: ";
     cin>>N;
 
-    int digit=N/10;
-    int rem=N%10;
-
-    int rev=10*digit+rem;
-    cout<<rev;
+    int rev = 0;
+    while(N>0){
+        int rem=N%10;
+        rev=rev*10+rem;
+        N=N/10;
+    }
+    cout<<"Reversed number is: "<<rev;
     return 0;
 }
